c09b728faddbf2f4e39057316843f5e60ca61d3f
user <user@vbox.univ-rouen.fr>
2025-09-11 09:29:21 -0400

    ajout version 4

--- genVM.sh
+++ genVM.sh
@@ -1,9 +1,52 @@
-VBoxManage createvm --name "DebianSAE51" --ostype "Debian_64" --register > /dev/null
-VBoxManage modifyvm DebianSAE51 --memory 4096 > /dev/null
-VBoxManage modifyvm DebianSAE51 --nic1 nat --boot1 net > /dev/null
-VBoxManage createhd --filename "$HOME/VirtualBox VMs/DebianSAE51/DebianSAE51.vdi" > /dev/null
-VBoxManage storagectl "DebianSAE51" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$HOME/VirtualBox VMs/DebianSAE51/DebianSAE51.vdi" > /dev/null
+RAM=4096
+DISK=64000
 
-read -p "test"
+if [ $# -lt 1 ]; then
+    echo "  L : Lister l’ensemble des machines enregistrées"
+    echo "  N : Ajouter une nouvelle machine"
+    echo "  S : Supprimer une machine"
+    echo "  D : Démarrer une machine"
+    echo "  A : Arreter une machine"
+    exit 1
+fi
 
-VBoxManage unregistervm "DebianSAE51" --delete > /dev/null
+ACTION=$1
+VMNAME=$2
+
+case "$ACTION" in
+    L)
+        echo "Liste des VMs :" VBoxManage list vms
+        ;;
+    N)
+        VMDISK="$HOME/VirtualBox VMs/$VMNAME/$VMNAME.vdi"
+
+        if VBoxManage list vms | grep -q "\"$VMNAME\""; then
+            VBoxManage unregistervm "$VMNAME" --delete > /dev/null
+        fi
+
+        VBoxManage createvm --name "$VMNAME" --ostype "Debian_64" --register > /dev/null
+        VBoxManage modifyvm "$VMNAME" --memory $RAM --nic1 nat --boot1 net > /dev/null
+        VBoxManage createhd --filename "$VMDISK" --size $DISK > /dev/null
+        VBoxManage storagectl "$VMNAME" --name "SATA Controller" --add sata --controller IntelAhci > /dev/null
+        VBoxManage storageattach "$VMNAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VMDISK" > /dev/null
+
+        echo "VM $VMNAME créée avec $RAM Mo RAM et disque $DISK Mo"
+        ;;
+    S)
+        if VBoxManage list vms | grep -q "\"$VMNAME\""; then 
+            VBoxManage unregistervm "$VMNAME" --delete > /dev/null
+        fi
+        echo "VM $VMNAME supprimée"
+        ;;
+    D)
+        VBoxManage startvm "$VMNAME" --type headless
+        ;;
+    A)
+        VBoxManage controlvm "$VMNAME" poweroff
+        ;;
+    *)
+        echo "Erreur : action invalide"
+        exit 1
+        ;;
+esac
+
