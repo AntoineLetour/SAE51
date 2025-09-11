04bc3815307d4cbf92994de88a97631d0d8ebe05
user <user@vbox.univ-rouen.fr>
2025-09-11 16:45:30 -0400

    ajout version 5

--- genVM.sh
+++ genVM.sh
@@ -3,6 +3,8 @@ DISK=64000
 ACTION=$1
 VMNAME=$2
 VMDISK="$HOME/VirtualBox VMs/$VMNAME/$VMNAME.vdi"
+USER_INFO="$USER"
+DATE_INFO=$(date --iso-8601)
 
 
 if [ $# -lt 1 ]; then
@@ -19,33 +21,64 @@ fi
 case "$ACTION" in
     L)
         echo "Liste des VMs :" 
-        VBoxManage list vms
+        VBoxManage list vms | while read line; do
+            VM="${line#\"}"
+            VM="${VM%%\"*}"
+            DATE_VBOX=$(VBoxManage getextradata "$VM" "date_info" 2> /dev/null)
+            USER_VBOX=$(VBoxManage getextradata "$VM" "user_info" 2> /dev/null)
+            echo "$VM  ${USER_VBOX}, ${DATE_VBOX}"
+        done
         ;;
-    N)
 
+    N)
         if VBoxManage list vms | grep -q "\"$VMNAME\""; then
-            VBoxManage unregistervm "$VMNAME" --delete > /dev/null
+           echo "Erreur : le nom de VM \"$VMNAME\" est déjà utilisé."
+           exit 1
+        fi
+        
+        if ! VBoxManage createvm --name "$VMNAME" --ostype "Debian_64" --register > /dev/null 2>&1; then
+           echo "Erreur : impossible de créer la VM"
+           exit 1
         fi
 
-        VBoxManage createvm --name "$VMNAME" --ostype "Debian_64" --register > /dev/null
-        VBoxManage modifyvm "$VMNAME" --memory $RAM --nic1 nat --boot1 net > /dev/null
-        VBoxManage createhd --filename "$VMDISK" --size $DISK > /dev/null
-        VBoxManage storagectl "$VMNAME" --name "SATA Controller" --add sata --controller IntelAhci > /dev/null
-        VBoxManage storageattach "$VMNAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VMDISK" > /dev/null
 
-        echo "VM $VMNAME créée avec $RAM Mo RAM et disque $DISK Go"
+        if ! VBoxManage modifyvm "$VMNAME" --memory $RAM --nic1 nat --boot1 net > /dev/null 2>&1; then
+           echo "Erreur : impossible de créer les caractéristiques de la VM"
+           exit 1
+        fi
+
+        if ! VBoxManage createhd --filename "$VMDISK" --size $DISK > /dev/null 2>&1 || \
+           ! VBoxManage storagectl "$VMNAME" --name "SATA Controller" --add sata --controller IntelAhci > /dev/null 2>&1 || \
+           ! VBoxManage storageattach "$VMNAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$VMDISK" > /dev/null 2>&1; then
+           echo "Erreur : impossible de créer le disque de la VM"
+           exit 1
+        fi
+
+        VBoxManage setextradata "$VMNAME" "date_info" "$DATE_INFO"
+        VBoxManage setextradata "$VMNAME" "user_info" "$USER_INFO"
+
+        echo "VM $VMNAME créée"
         ;;
     S)
         if VBoxManage list vms | grep -q "\"$VMNAME\""; then 
             VBoxManage unregistervm "$VMNAME" --delete > /dev/null
+        else
+            echo "VM $VMNAME supprimée"
         fi
-        echo "VM $VMNAME supprimée"
         ;;
     D)
-        VBoxManage startvm "$VMNAME" --type gui
+        if VBoxManage startvm "$VMNAME" --type gui > /dev/null 2>&1; then
+            echo "La VM est démarrée"
+        else
+            echo "Erreur : impossible de démarrer la VM"
+        fi
         ;;
     A)
-        VBoxManage controlvm "$VMNAME" poweroff
+        if  VBoxManage controlvm "$VMNAME" poweroff > /dev/null 2>&1; then
+            echo "La VM est éteinte"
+        else
+            echo "Erreur : impossible de d'éteindre la VM"
+        fi
         ;;
     *)
         echo "Erreur : action invalide"
